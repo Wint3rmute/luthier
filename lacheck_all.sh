074e9585eaea7e2ca49ed2963136ca459093165e
wint3rmute <mateusz.baczek1998@gmail.com>
2023-01-30 19:43:22 +0100

    Add lacheck to CI

---
+++ lacheck_all.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Runs lacheck on you .tex files, exits with error 1
+# if lacheck reports any errors with your tex spells
+
+for file in *.tex; do
+  if [[ "$file" = "Dyplom.tex" ]]; then
+    continue # Don't fix what's broken by PWr :)
+  fi
+
+  printf "Checking $file.. "
+
+  if [ $(lacheck "$file" | wc -l) != 0 ]; then
+    echo "$file has some problems!"
+    lacheck "$file" 
+    exit 1
+  fi
+
+  echo "OK"
+done
