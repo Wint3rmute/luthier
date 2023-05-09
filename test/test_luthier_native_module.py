d167f2fc1e1c94bb28f07fd943bfd08c788fd8df
wint3rmute <mateusz.baczek1998@gmail.com>
2023-05-10 00:21:52 +0200

    python formatting

--- test/test_luthier_native_module.py
+++ test/test_luthier_native_module.py
@@ -3,11 +3,12 @@ from typing import Any
 
 def test_graph_build(build_luthier: Any) -> None:
     from luthier import luthier
+
     graph = luthier.DspGraph()
+
 
 def test_graph_play(build_luthier: Any) -> None:
     from luthier import luthier
 
     graph = luthier.DspGraph()
     result = graph.play(100)
- 
