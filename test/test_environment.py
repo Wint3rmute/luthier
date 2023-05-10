074b9ea8e9b787f3a871738004282eebea7ea3ba
wint3rmute <m.baczek@satrev.space>
2023-05-10 21:46:22 +0200

    make ruff happy

--- test/test_environment.py
+++ test/test_environment.py
@@ -1,3 +1,7 @@
 def test_import_libraries_with_problematic_installation_procedures():
     import frechet_audio_distance
     import torch
+
+    # This makes ruff happy :)
+    assert frechet_audio_distance is not None
+    assert torch is not None
