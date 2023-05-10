9a7db73bddd5b0a1480a57db6a2eef561f162160
wint3rmute <m.baczek@satrev.space>
2023-05-10 20:19:33 +0200

    Make mypy happy

--- luthier/luthier.pyi
+++ luthier/luthier.pyi
@@ -5,7 +5,36 @@ F64NDArray = numpy.typing.NDArray[numpy.float64]
 
 class SineOscillator: ...
 
+class ADSR:
+    input_attack: float
+    input_sustain: float
+    input_decay: float
+    input_release: float
+
+class Multiplier:
+    input_scale: float
+    input_input: float
+    output_output: float
+
+class HarmonicMultiplier:
+    input_scale: float
+    input_input: float
+    output_output: float
+
 class DspGraph:
     def play(self, num_samples: int) -> F64NDArray: ...
     def draw(self) -> bytes: ...
-    def add_sine(self, sine: SineOscillator) -> None: ...
+    def patch(
+        self,
+        from_node_id: int,
+        from_output_name: str,
+        to_node_id: int,
+        to_input_name: str,
+    ) -> None: ...
+    def add_sine(self, sine: SineOscillator) -> int: ...
+    def add_adsr(self, adsr: ADSR) -> int: ...
+    def add_multiplier(self, multiplier: Multiplier) -> int: ...
+    def add_harmonic_multiplier(self, multiplier: HarmonicMultiplier) -> int: ...
+
+    speaker_node_id: int
+    amp_adsr_node_id: int
