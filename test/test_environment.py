28a952a2f82fcb75f7df57873d658718b32794df
wint3rmute <m.baczek@satrev.space>
2023-05-11 16:33:30 +0200

    remove frechet audio distance as serves a different purpose and is not fit for my case

--- test/test_environment.py
+++
@@ -1,7 +0,0 @@
-def test_import_libraries_with_problematic_installation_procedures() -> None:
-    import frechet_audio_distance
-    import torch
-
-    # This makes ruff happy :)
-    assert frechet_audio_distance is not None
-    assert torch is not None
