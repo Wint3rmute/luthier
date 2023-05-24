15181cab91c6ae8f40acafe141faaf4a687dcda1
wint3rmute <m.baczek@satrev.space>
2023-05-24 17:19:21 +0200

    make mypy heppy

--- test/test_python_dsp.py
+++ test/test_python_dsp.py
@@ -4,12 +4,12 @@ import pytest
 from luthier.dsp import Sample
 
 
-def test_mfcc_distance():
+def test_mfcc_distance() -> None:
     s = Sample(np.zeros(100))
     assert s.mfcc_distance(s) == 0
 
 
-def test_mfcc_distance_fails_on_different_sample_sizes():
+def test_mfcc_distance_fails_on_different_sample_sizes() -> None:
     s1 = Sample(np.zeros(10))
     s2 = Sample(np.zeros(11))
 
@@ -17,6 +17,6 @@ def test_mfcc_distance_fails_on_different_sample_sizes():
         s1.mfcc_distance(s2)
 
 
-def test_spectrogram_distance():
+def test_spectrogram_distance() -> None:
     s = Sample(np.zeros(4096 * 10))
     assert s.spectrogram_distance(s) == 0
